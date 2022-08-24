1ebc306d63a7cacdf434a81a47c431e1604bd95b
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-08-23 19:48:18 -0500

    updated schema

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT =3000;
-const Service = require("./models/salon.js");
+const Service = require("./models/services.js");
 const methodOverride = require('method-override');
 // SETUP MONGOOSE
 const mongoose = require("mongoose");
+                               // ip:address// data if you have
 mongoose.connect("mongodb://127.0.0.1:27017/basiccrud");
 mongoose.connection.once("open", () => {
    console.log("Connected to Mongo");
@@ -14,6 +15,8 @@ mongoose.connection.once("open", () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+
+// ROUTE MAIN
 app.get("/", (req,res) => {
    const today = new Date();
    res.send(`
@@ -26,6 +29,48 @@ app.get("/", (req,res) => {
    `);
 })
 
+app.get("/seed", (req, res) => {
+   const newServices = 
+   [
+      {
+         service: "hairCuts",
+         description: "men",
+         price: 20,
+         qty: 1,
+
+      },
+      {
+         service: "hairCuts",
+         description: "women",
+         price: 30,
+         qty: 1,
+      },
+      {
+         service: "hairColor",
+         description: "men",
+         price: 20,
+         qty: 1,
+      }
+]
+})
+
+// INDEX
+app.get("/services",(req,res) => {
+   console.log("Index route working")
+   // Service.find({}, (err, services))
+   // res.render("index.ejs", {
+   //    allServices :services})
+}) 
+
+// SHOW
+
+// NEW/CREATE
+
+// EDIT
+
+// DELETE
+
+//UPDATE
 
 
 
