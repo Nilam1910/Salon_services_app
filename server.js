bdcc53b4f46c74919a0970a5d5131e1a2af94572
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-08-26 19:38:03 -0500

    routes done

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
-const PORT =3000;
 const Service = require("./models/services.js");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
+// Environment Variable
+require("dotenv").config() // you have to be obey the variable of port
+// console.log(require("dotenv").config()) //just to check
+// console.log(process.env) // check error and its working
+const PORT = process.env.PORT // check in bottom too
 // SETUP MONGOOSE
 const mongoose = require("mongoose");
+const mongoURI = process.env.MONGODB_URI
                                // ip:address// data if you have
-mongoose.connect("mongodb://127.0.0.1:27017/services");
+
+mongoose.connect(mongoURI);
 mongoose.connection.once("open", () => {
    console.log("Connected to Mongo");
-})
+}); 
 
 //MIDDLEWARE
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"))
 
 // ROUTE MAIN
 app.get("/", (req,res) => {
@@ -70,18 +77,62 @@ app.get("/services", async (req,res) => {
    console.log("after")
 }) 
 
+// NEW
+app.get("/services/new", (req,res) => {
+   res.render("new.ejs")
+})
+
 // SHOW
+app.get("/services/:id", async (req,res) => {
+   Service.findById(req.params.id, (err, foundService) => {
 
-// NEW/CREATE
+      res.render("show.ejs",{
+         singleService: foundService
+      })
+   })
+   
+})
 
-// EDIT
+//CREATE (POST)
+   app.post("/services",(req,res) => {
+      Service.create(req.body, (error, createdService) => {
+         if(error){
+            console.log("error", error)
+            res.send(error)
+         }  else{
+            res.redirect("/services")
+         }
+      });
+
+   })
 
 // DELETE
+   app.delete("/services/:id", (req,res) =>{
+      // res.send('deleting item') //  working
+      Service.findByIdAndRemove(req.params.id, (err, data) =>{
+         if(err){
+            console.log(err)
+         }
+         res.redirect("/services");
+      });
+   });
 
+// EDIT 
+   app.get("/services/:id/edit", (req,res) =>{
+      Service.findById(req.params.id, (err, foundServices) =>{
+         res.render("edit.ejs", {EditService: foundServices }
+         );
+      });
+   });
+
+  
 //UPDATE
-
-
-
+app.put("/services/:id", (req,res) =>{
+   Service.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) =>{
+      // res.send(updatedModel) worked
+      res.redirect("/services"); // back to index page
+   });
+});
 
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
@@ -91,4 +142,16 @@ app.listen(PORT, () => {
 
 
 
+// {/* <h2><%= allServices[i].description %></h2>
+//             <h2><%= allServices[i].price %></h2>
+//             <h2><%= allServices[i].qty %></h2>  */}
 
+// ************ for index
+
+            // <form action="/services/<%=allServices[i].id%>?_method=DELETE" method="POST">
+            // <h2><%= allServices[i].description %> </h2>
+            // <h2><%= allServices[i]price%></h2>
+            // <h2><%= allServices[i]qty%></h2>
+            // <input type="submit" value="DELETE">
+            // </form>
+            // <a href="/services/<%= allServices[i].id%>/edit">Edit</a>
