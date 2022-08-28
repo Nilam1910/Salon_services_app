e6af1cd32b9cb0e7cf5338f16443d510bd1ca486
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-08-28 17:51:58 -0500

    create new use works with error message

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,42 @@
 const express = require("express");
+const bcrypt = require("bcrypt")
+
+// we need our User model
+const User = require("../models/users") // you do not have to .js
+
+
 const router = express.Router();
 
-// router.get("/", (req,res) => {
-//    res.send("USER CONTROLLER WORKS")
-// })
+router.get("/", (req,res) => {
+   res.send("USER CONTROLLER WORKS")
+})
 
 router.get("/register", (req,res) => {
    res.render("users/register.ejs")
+})
+
+router.post("/register", (req,res) => {
+   // encrypt passwords, use bcrypt library, import the library at the top of our file and first things we need to generate salt
+   const salt = bcrypt.genSaltSync(10)
+   // salt is a random garbage we add to our encrypted passwords
+   // the number we pass to genSaltSync determines how much salt we are adding, the higher the number the more secure, but the longer its takes
+   // generate the actual hashed password
+   // console.log(req.body) // worked
+   req.body.password = bcrypt.hashSync(req.body.password, salt)
+   // console.log(req.body) // worked
+
+   // see if somebody else already has this username
+   User.findOne({username: req.body.username}, (err,userExists) => {
+      if(userExists){
+         res.send("That's UserName is Taken")  // need to render back to signup page
+      }  else  {
+         User.create(req.body, (err, createdUser) =>{
+            console.log(createdUser)
+            res.send("user created") //need to render back to signup page
+         })
+      }
+   })
+
 })
 
 module.exports = router
--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
    username: {type: String, require: true, unique: true},
    password: {type: String, require: true },
-   email: {type: string}
+   email: {type: String}
 })
 
 const User = mongoose.model("User" , userSchema)
